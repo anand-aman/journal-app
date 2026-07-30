02beea810b87580aac964bb3fc1a5fe33722be40
anand-aman <itsamananand@gmail.com>
2026-07-31 00:47:00 +0530

    controller setup

---
+++ src/main/java/com/curiodesk/journalapp/entity/JournalEntry.java
@@ -0,0 +1,14 @@
+package com.curiodesk.journalapp.entity;
+
+import lombok.Data;
+
+@Data
+public class JournalEntry {
+
+    private long id;
+
+    private String title;
+
+    private String content;
+
+}
